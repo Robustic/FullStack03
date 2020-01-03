af702971503f0e380f5f60b2caa25c8289243777
Juha Malinen <juha.malinen@helsinki.fi>
2020-01-03 20:30:03 +0200

    Osa 3.12

---
+++ mongo.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+    console.log('give password as argument')
+    process.exit(1)
+}
+
+const password = process.argv[2]
+
+const url =
+    `mongodb+srv://ClusteriDBuser:${password}@clusteri-nm3ye.mongodb.net/person-app?retryWrites=true&w=majority`
+
+mongoose.connect(url, { useNewUrlParser: true })
+
+const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+if (process.argv.length < 5) {    
+    Person.find({}).then(result => {
+        console.log(`phonebook:`)
+        result.forEach(person => {
+            console.log(`${person.name} ${person.number}`)
+        })
+        mongoose.connection.close()
+    })
+} else {
+    const argName = process.argv[3]
+    const argNumber = process.argv[4]    
+
+    const person = new Person({
+        name: argName,
+        number: argNumber,
+    })
+
+    person.save().then(response => {
+        console.log(`added ${argName} number ${argNumber} to phonebook`);
+        mongoose.connection.close();
+    })
+}

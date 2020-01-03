bd0d105ac560951bdd83a7553dc53541a46f4c91
Juha Malinen <juha.malinen@helsinki.fi>
2020-01-03 15:40:59 +0200

    uibuild

---
+++ build/precache-manifest.7e9403a361ddb09bd26291b7d4374c81.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92101138fd694f913d0b657baa62e69a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "36fb2df8576e0223e14a",
+    "url": "/static/js/2.70662f98.chunk.js"
+  },
+  {
+    "revision": "098d96cd5327dd3fc6707a77e047f86b",
+    "url": "/static/js/2.70662f98.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ae37df706e34f5e51f49",
+    "url": "/static/js/main.76579cee.chunk.js"
+  },
+  {
+    "revision": "ec2ac39fbc5044a4cece",
+    "url": "/static/js/runtime-main.97c5a5d6.js"
+  }
+]);

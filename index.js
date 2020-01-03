7708b42d49dfcc7e17347abc05d048784bb1d698
Juha Malinen <juha.malinen@helsinki.fi>
2020-01-03 14:47:13 +0200

    Osa 3.10 after cleaning

---
+++ index.js
@@ -0,0 +1,116 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function (req, res) {
+    res.send('<p>TaallaOllaan</p>');
+});
+
+app.listen(process.env.PORT || 3001, function () {
+    console.log('Node app is working!');
+});
+
+// const express = require('express')
+// const app = express()
+// // const morgan = require('morgan')
+// // const bodyParser = require('body-parser')
+// // const cors = require('cors')
+
+// // morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+// // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+
+// // app.use(bodyParser.json())
+// // app.use(cors())
+
+// // let persons = [
+// //     {
+// //         "name": "Arto Hellas",
+// //         "number": "040-123456",
+// //         "id": 1
+// //     },
+// //     {
+// //         "name": "Ada Lovelace",
+// //         "number": "39-44-5323523",
+// //         "id": 2
+// //     },
+// //     {
+// //         "name": "Dan Abramov",
+// //         "number": "12-43-234345",
+// //         "id": 3
+// //     },
+// //     {
+// //         "name": "Mary Poppendieck",
+// //         "number": "39-23-6423122",
+// //         "id": 4
+// //     }
+// // ]
+
+// // app.get('/info', (req, res) => {
+// //     res.send('<p>Phonebook has info for ' + persons.length + ' people</p><p>' + Date() + '</p>')
+// // })
+
+// // app.get('/api/persons', (req, res) => {
+// //     res.json(persons)
+// // })
+
+// // app.get('/api/persons/:id', (request, response) => {
+// //     const id = Number(request.params.id)
+// //     const person = persons.find(person => person.id === id)
+
+// //     if (person) {
+// //         response.json(person)
+// //     } else {
+// //         response.status(404).end()
+// //     }
+// // })
+
+// // const generateId = () => {
+// //     const id = Math.floor(Math.random() * 1000000000)
+// //     return id
+// // }
+
+// // app.post('/api/persons', (request, response) => {
+// //     const body = request.body
+
+// //     if (!body.name || !body.number) {
+// //         return response.status(400).json({
+// //             error: 'name or number missing'
+// //         })
+// //     }
+
+// //     const personExistsList = persons.filter(person => (person.name === body.name))
+// //     if (personExistsList.length > 0) {
+// //         return response.status(400).json({
+// //             error: 'name already exists'
+// //         })
+// //     }
+
+// //     const person = {
+// //         name: body.name,
+// //         number: body.number,
+// //         id: generateId(),
+// //     }
+// //     persons = persons.concat(person)
+// //     response.json(person)
+// // })
+
+// // app.delete('/api/persons/:id', (request, response) => {
+// //     const id = Number(request.params.id)
+// //     persons = persons.filter(person => person.id !== id)
+
+// //     response.status(204).end()
+// // })
+
+// app.get('/', (req, res) => {
+//     res.send('<p>TaallaOllaan</p>')
+// })
+
+// app.get('/info', (req, res) => {
+//     res.send('<p>TaallaOllaanToisessaKohtaa</p>')
+// })
+
+// const PORT = process.env.PORT || 3001
+// app.listen(PORT, () => {
+//   //console.log(`Server running on port ${PORT}`)
+// })
+
+// module.exports = app

3788ce5822a7a7933d536bc569918c17cfdf74db
Juha Malinen <juha.malinen@helsinki.fi>
2020-01-03 14:55:31 +0200

    Osa 3.10 moving to Heroku

--- index.js
+++ index.js
@@ -1,116 +1,114 @@
-var express = require('express');
-var app = express();
+// var express = require('express');
+// var app = express();
+// 
+// app.get('/', function (req, res) {
+//     res.send('<p>TaallaOllaan</p>');
+// });
+// 
+// app.listen(process.env.PORT || 3001, function () {
+//     console.log('Node app is working!');
+// });
 
-app.get('/', function (req, res) {
-    res.send('<p>TaallaOllaan</p>');
-});
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const bodyParser = require('body-parser')
+const cors = require('cors')
 
-app.listen(process.env.PORT || 3001, function () {
-    console.log('Node app is working!');
-});
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-// const express = require('express')
-// const app = express()
-// // const morgan = require('morgan')
-// // const bodyParser = require('body-parser')
-// // const cors = require('cors')
+app.use(bodyParser.json())
+app.use(cors())
 
-// // morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
-// // app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
+let persons = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    }
+]
 
-// // app.use(bodyParser.json())
-// // app.use(cors())
+app.get('/info', (req, res) => {
+    res.send('<p>Phonebook has info for ' + persons.length + ' people</p><p>' + Date() + '</p>')
+})
 
-// // let persons = [
-// //     {
-// //         "name": "Arto Hellas",
-// //         "number": "040-123456",
-// //         "id": 1
-// //     },
-// //     {
-// //         "name": "Ada Lovelace",
-// //         "number": "39-44-5323523",
-// //         "id": 2
-// //     },
-// //     {
-// //         "name": "Dan Abramov",
-// //         "number": "12-43-234345",
-// //         "id": 3
-// //     },
-// //     {
-// //         "name": "Mary Poppendieck",
-// //         "number": "39-23-6423122",
-// //         "id": 4
-// //     }
-// // ]
+app.get('/api/persons', (req, res) => {
+    res.json(persons)
+})
 
-// // app.get('/info', (req, res) => {
-// //     res.send('<p>Phonebook has info for ' + persons.length + ' people</p><p>' + Date() + '</p>')
-// // })
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = persons.find(person => person.id === id)
 
-// // app.get('/api/persons', (req, res) => {
-// //     res.json(persons)
-// // })
+    if (person) {
+        response.json(person)
+    } else {
+        response.status(404).end()
+    }
+})
 
-// // app.get('/api/persons/:id', (request, response) => {
-// //     const id = Number(request.params.id)
-// //     const person = persons.find(person => person.id === id)
+const generateId = () => {
+    const id = Math.floor(Math.random() * 1000000000)
+    return id
+}
 
-// //     if (person) {
-// //         response.json(person)
-// //     } else {
-// //         response.status(404).end()
-// //     }
-// // })
+app.post('/api/persons', (request, response) => {
+    const body = request.body
 
-// // const generateId = () => {
-// //     const id = Math.floor(Math.random() * 1000000000)
-// //     return id
-// // }
+    if (!body.name || !body.number) {
+        return response.status(400).json({
+            error: 'name or number missing'
+        })
+    }
 
-// // app.post('/api/persons', (request, response) => {
-// //     const body = request.body
+    const personExistsList = persons.filter(person => (person.name === body.name))
+    if (personExistsList.length > 0) {
+        return response.status(400).json({
+            error: 'name already exists'
+        })
+    }
 
-// //     if (!body.name || !body.number) {
-// //         return response.status(400).json({
-// //             error: 'name or number missing'
-// //         })
-// //     }
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: generateId(),
+    }
+    persons = persons.concat(person)
+    response.json(person)
+})
 
-// //     const personExistsList = persons.filter(person => (person.name === body.name))
-// //     if (personExistsList.length > 0) {
-// //         return response.status(400).json({
-// //             error: 'name already exists'
-// //         })
-// //     }
+app.delete('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
 
-// //     const person = {
-// //         name: body.name,
-// //         number: body.number,
-// //         id: generateId(),
-// //     }
-// //     persons = persons.concat(person)
-// //     response.json(person)
-// // })
-
-// // app.delete('/api/persons/:id', (request, response) => {
-// //     const id = Number(request.params.id)
-// //     persons = persons.filter(person => person.id !== id)
-
-// //     response.status(204).end()
-// // })
+    response.status(204).end()
+})
 
 // app.get('/', (req, res) => {
 //     res.send('<p>TaallaOllaan</p>')
 // })
-
+// 
 // app.get('/info', (req, res) => {
 //     res.send('<p>TaallaOllaanToisessaKohtaa</p>')
 // })
 
-// const PORT = process.env.PORT || 3001
-// app.listen(PORT, () => {
-//   //console.log(`Server running on port ${PORT}`)
-// })
-
-// module.exports = app
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
